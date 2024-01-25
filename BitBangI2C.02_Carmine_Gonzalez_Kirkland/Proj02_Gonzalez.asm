ddf4ea1029b0fc8f409e0b7e4ba4747f178306d4
Lance G <lancedgonzalez@gmail.com>
2024-01-25 14:02:21 -0700

    v01: started scl timer compare, ISR not finished

--- BitBangI2C.02_Carmine_Gonzalez_Kirkland/Proj02_Gonzalez.asm
+++ BitBangI2C.02_Carmine_Gonzalez_Kirkland/Proj02_Gonzalez.asm
@@ -3,12 +3,18 @@
 ; EELE465
 ; Written by: Lance Gonzalez
 ; Project 02 - Jan 25 2024
-; Heartbeat LED
+; Bit Banging I2C 
 ;
+;   Ports:
+;       P3.6 - I2C SDA
+;       P5.2 - I2C SCL 
+;
+;   Registers:
+;       R4: I2C transmit byte
 ;
 ;
 ; Version History:
-;   v01 - 
+;   v01 - Created timer compart interrupt for SCL PWM
 ;-------------------------------------------------------------------------------
 
             .cdecls C,LIST,"msp430.h"       ; Include device header file
@@ -27,12 +33,51 @@
 RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
 StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 
+;~~ INITIALIZATION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+init: 
+    ;^ Handles port, register, and other initialization
 
+    ; Port Setup
+        bis.b   #BIT0, &P1DIR               ; P1.0 -> output
+        bic.b   #BIT0, &P1OUT
+        bic.b   #LOCKLPM5, &PM5CTL0         ; disable low power mode 
+
+    ; Timer Setup
+        bis.w   #TBCLR, &TB0CTL             ; Clear TB0
+        bis.w   #TBSSEL__SMCLK, &TB0CTL     ; SMCLK
+        bis.w   #MC_UP, &TB0CTL             ; UP 
+
+    ; Timer Compare Reg
+        mov.w   #01BE7h, &TB0CCR0           ; 7143d -> 7.1428e-5 s
+        mov.w   #0BB8h, &TB0CCR1            ; 3000d -> 3e-5 s 
+
+        ;Interrupt enable + flag clear
+        bis.w   #CCIE, &TB0CCTL0
+        bic.w   #CCIFG, &TB0CCTL0
+        
+        ;Interrupt enable + flag clear
+        bis.w   #CCIE, &TB0CCTL1
+        bic.w   #CCIFG, &TB0CCTL1
+
+        bis.w   #GIE, SR                    ; Enable maskable
+
+
+;~~ MAIN FUNCTION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
+main:   
+    ;^ Infinite Loop
+            jmp     main
 
-                                            
+;~~ INTERRUPT SERVICE ROUTINES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+;-------------------------------------------------------------------------------
+; ISR - TIMERB0_CLOCK_PWM:
+;-------------------------------------------------------------------------------
+ISR_TB0_ClockPWM:
+    ;^ PWM at 14 kHz, duty = 42%
+            reti
+;----------------- END TIMERB0_CLOCK_PWM ---------------------------------------
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
